e9f6daf0e029b6840cc528b467d3716c0c2df124
sylathas <abate.niccolo@gmail.com>
2020-02-01 17:10:00 +0100

    Game

---
+++ assets/tilemaps/finestre.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Finestre" tilewidth="64" tileheight="64" tilecount="208" columns="13">
+ <image source="../tilesets/Finestre.png" width="832" height="1024"/>
+ <tile id="79">
+  <animation>
+   <frame tileid="79" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="144" duration="100"/>
+   <frame tileid="147" duration="100"/>
+   <frame tileid="150" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="80">
+  <animation>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="145" duration="100"/>
+   <frame tileid="148" duration="100"/>
+   <frame tileid="151" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="92">
+  <animation>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="95" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="157" duration="100"/>
+   <frame tileid="160" duration="100"/>
+   <frame tileid="163" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="93">
+  <animation>
+   <frame tileid="93" duration="100"/>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="158" duration="100"/>
+   <frame tileid="161" duration="100"/>
+   <frame tileid="164" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="105">
+  <animation>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="111" duration="100"/>
+   <frame tileid="114" duration="100"/>
+   <frame tileid="170" duration="100"/>
+   <frame tileid="173" duration="100"/>
+   <frame tileid="176" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="106">
+  <animation>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="109" duration="100"/>
+   <frame tileid="112" duration="100"/>
+   <frame tileid="115" duration="100"/>
+   <frame tileid="171" duration="100"/>
+   <frame tileid="174" duration="100"/>
+   <frame tileid="177" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="118">
+  <animation>
+   <frame tileid="118" duration="100"/>
+   <frame tileid="121" duration="100"/>
+   <frame tileid="124" duration="100"/>
+   <frame tileid="127" duration="100"/>
+   <frame tileid="183" duration="100"/>
+   <frame tileid="186" duration="100"/>
+   <frame tileid="189" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="119">
+  <animation>
+   <frame tileid="119" duration="100"/>
+   <frame tileid="122" duration="100"/>
+   <frame tileid="125" duration="100"/>
+   <frame tileid="128" duration="100"/>
+   <frame tileid="184" duration="100"/>
+   <frame tileid="187" duration="100"/>
+   <frame tileid="190" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/tilesets/Finale Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kenney-tileset-64px-extruded" tilewidth="64" tileheight="64" tilecount="374" columns="22">
+ <image source="kenney-tileset-64px-extruded.png" width="1452" height="1122"/>
+</tileset>
